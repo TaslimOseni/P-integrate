c6a5a60ea6e04ed750c2f580426852beeb201fa2
TaslimOseni <tasleemoseni@gmail.com>
2018-08-01 19:18:58 +0100

    Finished back-end, not reviewed...

--- app/src/main/java/apps/dabinu/com/p_integrate/GooglePayActivity.java
+++ app/src/main/java/apps/dabinu/com/p_integrate/GooglePayActivity.java
@@ -1,15 +1,27 @@
 package apps.dabinu.com.p_integrate;
 
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.wallet.AutoResolveHelper;
 import com.google.android.gms.wallet.CardRequirements;
 import com.google.android.gms.wallet.IsReadyToPayRequest;
+import com.google.android.gms.wallet.PaymentData;
 import com.google.android.gms.wallet.PaymentDataRequest;
+import com.google.android.gms.wallet.PaymentMethodToken;
 import com.google.android.gms.wallet.PaymentMethodTokenizationParameters;
 import com.google.android.gms.wallet.PaymentsClient;
 import com.google.android.gms.wallet.ShippingAddressRequirements;
@@ -21,10 +33,13 @@ import java.math.RoundingMode;
 import java.util.Arrays;
 import java.util.List;
 
+
+
 public class GooglePayActivity extends AppCompatActivity{
 
 
-/*    Before you continue, make sure you add these dependecies to your build.gradle file:
+/*
+    Before you continue, make sure you add these dependecies to your build.gradle file:
 
     implementation 'com.google.android.gms:play-services-wallet:15.0.1'
     implementation 'com.android.support:support-v4:27.1.1'
@@ -34,28 +49,167 @@ public class GooglePayActivity extends AppCompatActivity{
     private PaymentsClient mPaymentsClient;
     private static final int LOAD_PAYMENT_DATA_REQUEST_CODE = 991; // Randomly-picked code [You can use this].
 
+//    Let's create an object for our inner class PaymentUtil
+      PaymentsUtil paymentsUtil = new PaymentsUtil();
+
+//    This is a progress dialog
+    ProgressDialog progressDialog;
+
+    //Price, make sure you include shipping and taxes
+    String price = "2000";
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_pay);
 
+        //Initialize Payment client
         mPaymentsClient = new PaymentsUtil().createPaymentsClient(this);
-//        checkIsReadyToPay();
 
+        // It's recommended to create the PaymentsClient object inside of the onCreate method.
+        mPaymentsClient = paymentsUtil.createPaymentsClient(this);
+
+        progressDialog = new ProgressDialog(this);
 
         Button pay = findViewById(R.id.pay_button);
 
         pay.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
+                progressDialog.setMessage("Checking whether Google pay is available on your device...");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
 
+                checkWhetherGooglePayIsAvailable(v);
             }
         });
+
+    }
+
+
+    private void checkWhetherGooglePayIsAvailable(final View v){
+        // The call to isReadyToPay is asynchronous and returns a Task. We need to provide an
+        // OnCompleteListener to be triggered when the result of the call is known.
+        paymentsUtil.isReadyToPay(mPaymentsClient).addOnCompleteListener(
+                new OnCompleteListener<Boolean>() {
+                    public void onComplete(Task<Boolean> task) {
+                        try{
+                            // If isReadyToPay returned true, show the button and hide the "checking" text. Otherwise,
+                            // notify the user that Pay with Google is not available.
+                            // Please adjust to fit in with your current user flow. You are not required to explicitly
+                            // let the user know if isReadyToPay returns false.
+
+                            if(task.getResult(ApiException.class)){
+                                progressDialog.setMessage("Processing payment...");
+                                requestPayment(v);
+                            }
+                            else{
+                                progressDialog.cancel();
+                                progressDialog.dismiss();
+                                new AlertDialog.Builder(getApplicationContext())
+                                        .setMessage("Sorry, Google play is not available on this device.")
+                                        .setCancelable(true)
+                                        .show();
+                            }
+                        } catch (ApiException exception){
+                            // Process error
+                            Log.w("isReadyToPay failed", exception);
+
+                            new AlertDialog.Builder(getApplicationContext())
+                                    .setMessage("Sorry, An error occurred.")
+                                    .setCancelable(true)
+                                    .show();
+                        }
+                    }
+                });
+    }
+
+
+    // This method is called when the Pay with Google button is clicked.
+    public void requestPayment(View view){
+
+        TransactionInfo transaction = paymentsUtil.createTransaction(price);
+        PaymentDataRequest request = paymentsUtil.createPaymentDataRequest(transaction);
+        Task<PaymentData> futurePaymentData = mPaymentsClient.loadPaymentData(request);
+
+        // Since loadPaymentData may show the UI asking the user to select a payment method, we use
+        // AutoResolveHelper to wait for the user interacting with it. Once completed,
+        // onActivityResult will be called with the result.
+        AutoResolveHelper.resolveTask(futurePaymentData, this, LOAD_PAYMENT_DATA_REQUEST_CODE);
     }
 
 
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        switch (requestCode) {
+            case LOAD_PAYMENT_DATA_REQUEST_CODE:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        progressDialog.cancel();
+                        progressDialog.dismiss();
+                        PaymentData paymentData = PaymentData.getFromIntent(data);
+                        handlePaymentSuccess(paymentData);
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        // Nothing to here normally - the user simply cancelled without selecting a
+                        // payment method.
+                        progressDialog.cancel();
+                        progressDialog.dismiss();
+                        Toast.makeText(getApplicationContext(), "Cancelled", Toast.LENGTH_LONG).show();
+                        break;
+                    case AutoResolveHelper.RESULT_ERROR:
+                        progressDialog.cancel();
+                        progressDialog.dismiss();
+                        Status status = AutoResolveHelper.getStatusFromIntent(data);
+                        handleError(status.getStatusCode());
+                        break;
+                }
+
+
+                break;
+        }
+    }
+
+
+    private void handlePaymentSuccess(PaymentData paymentData){
+        // PaymentMethodToken contains the payment information, as well as any additional
+        // requested information, such as billing and shipping address.
+        //
+        // Refer to your processor's documentation on how to proceed from here.
+        PaymentMethodToken token = paymentData.getPaymentMethodToken();
+
+        // getPaymentMethodToken will only return null if PaymentMethodTokenizationParameters was
+        // not set in the PaymentRequest.
+        if (token != null) {
+            // If the gateway is set to example, no payment information is returned - instead, the
+            // token will only consist of "examplePaymentMethodToken".
+            if (token.getToken().equals("examplePaymentMethodToken")){
+                AlertDialog alertDialog = new AlertDialog.Builder(this)
+                        .setTitle("Warning")
+                        .setMessage("Gateway name set to \"example\" - please modify " +
+                                "Constants.java and replace it with your own gateway.")
+                        .setPositiveButton("OK", null)
+                        .create();
+                alertDialog.show();
+            }
+
+            String billingName = paymentData.getCardInfo().getBillingAddress().getName();
+            Toast.makeText(this, "Successfully received payment data!", Toast.LENGTH_LONG).show();
+
+            // Use token.getToken() to get the token string.
+            Log.d("PaymentData", "PaymentMethodToken received");
+        }
+    }
+
+
+    private void handleError(int statusCode){
+        // At this stage, the user has already seen a popup informing them an error occurred.
+        // Normally, only logging is required.
+        // statusCode will hold the value of any constant from CommonStatusCode or one of the
+        // WalletConstants.ERROR_CODE_* constants.
+        Log.w("loadPaymentData failed", String.format("Error code: %d", statusCode));
+    }
 
 
 
@@ -69,7 +223,7 @@ public class GooglePayActivity extends AppCompatActivity{
      * existence. Please consult the documentation to learn more and feel free to remove ones not
      * relevant to your implementation.
      */
-    public class PaymentsUtil {
+    public class PaymentsUtil{
         private final BigDecimal MICROS = new BigDecimal(1000000d);
 
         private PaymentsUtil() {
@@ -215,9 +369,7 @@ public class GooglePayActivity extends AppCompatActivity{
 
 
 
-
-
-    public class Constants {
+    public class Constants{
         // This file contains several constants you must edit before proceeding.
         //
         // Please take a look at PaymentsUtil.java to see where the constants are used and to
@@ -290,7 +442,6 @@ public class GooglePayActivity extends AppCompatActivity{
         }
 
     }
-
 
 
 }
