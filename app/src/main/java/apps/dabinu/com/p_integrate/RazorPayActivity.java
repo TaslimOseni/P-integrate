c842923fe219e2f5934f3bed587484caf402fb14
TaslimOseni <tasleemoseni@gmail.com>
2018-08-04 18:42:11 +0100

    Init razorpay branch

---
+++ app/src/main/java/apps/dabinu/com/p_integrate/RazorPayActivity.java
@@ -0,0 +1,13 @@
+package apps.dabinu.com.p_integrate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RazorPayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_razor_pay);
+    }
+}
