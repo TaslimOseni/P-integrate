42a578b3a2d3e822dfd90c2079aff37a278f0cf2
TaslimOseni <tasleemoseni@gmail.com>
2018-08-18 16:07:45 +0100

    Implemented Vigipay XML

---
+++ app/src/main/java/apps/dabinu/com/p_integrate/VigiPayActivity.java
@@ -0,0 +1,13 @@
+package apps.dabinu.com.p_integrate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VigiPayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vigi_pay);
+    }
+}
